8d68ed27b5537987fd654211653fa5e061a66871
Dane Walton <dawalton@microsoft.com>
2021-12-16 08:24:07 -0800

    update prov client CMake

--- build_all/linux/run_tests.sh
+++ build_all/linux/run_tests.sh
@@ -27,9 +27,27 @@ sudo ldconfig
 
 if [[ "$RUN_VALGRIND" == "run_valgrind" ]] ;
 then
-  #use doctored openssl
+  # Use doctored openssl
   export LD_LIBRARY_PATH=/usr/local/ssl/lib
-  ctest -j $TEST_CORES --output-on-failure --schedule-random
+
+  # Run unit tests
+  ctest -j $TEST_CORES --output-on-failure --schedule-random -R "_ut"
+  
+  # E2E tests are divided up between the basic and all the valgrind options
+  # Having them together risks multiple versions of the same test being run at
+  # the same time and causing false negatives.
+
+  # Run basic e2e tests
+  ctest -j $TEST_CORES --output-on-failure --schedule-random -E "_valgrind|_helgrind|_drd|_ut"
+  
+  # Run valgrind e2e tests
+  ctest -j $TEST_CORES --output-on-failure --schedule-random -R valgrind -E "_ut"
+  
+  # Run helgrind e2e tests
+  ctest -j $TEST_CORES --output-on-failure --schedule-random -R helgrind -E "_ut"
+  
+  # Run drd e2e tests
+  ctest -j $TEST_CORES --output-on-failure --schedule-random -R drd -E "_ut"
   export LD_LIBRARY_PATH=
 else
   ctest -j $TEST_CORES -C "Debug" --output-on-failure --schedule-random
